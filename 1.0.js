58ea9e8a82eb9e79ccb14f21d0e02fa892048e10
Bruno_P <presti.bruno88@gmail.com>
2021-09-23 14:28:00 +0200

    Initialization to js

--- 1.0.js
+++ 1.0.js
@@ -1,3 +1,19 @@
-let age = 33;
-age += 1;
+let age1 = 33;
+age = age1 + 1;
+console.log(age);
+
+let age2 = 33;
+age = age2 - 1;
+console.log(age);
+
+let age3 = 33;
+age = age3 * 2;
+console.log(age);
+
+let age4 = 33;
+age = age4 / 2;
+console.log(age);
+
+let age5 = 33;
+age = age5 % 10;
 console.log(age);
