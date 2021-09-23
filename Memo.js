43bbb70788b6e8e492e2af512094dd2e0dfe94a5
Bruno_P <presti.bruno88@gmail.com>
2021-09-23 16:11:37 +0200

    Initialization to js

---
+++ Memo.js
@@ -0,0 +1,53 @@
+/* const readlineSync = require("readline-sync");
+
+let userName = readlineSync.question("Can you give me your name please?");
+console.log("Hello " + userName);
+
+let age = 23;
+
+let sisterAge = age + 11;
+
+age += 12; // age = age + 12;
+
+console.log(age);
+
+let name = "Jean";
+let nameSister = "Emma";
+console.log("My name is " + name);
+
+let userName = readlineSync.question("What's your name?");
+console.log("He's called " + userName);
+
+let htmlBadges = new Number(readlineSync.question('How many HTML badges do you have?'));
+
+let cssBadges = new Number(readlineSync.question('How many CSS badges do you have?'));
+
+let totalBadges = htmlBadges + cssBadges;
+
+console.log('Woaw, you have ' + totalBadges + "!");
+
+console.log('10' + '5'); // '105' => string type => Quand c'est entre ' ', ça écrit tel quel, ça n'additionne pas
+
+console.log(10 + 5); // 15 => number type => sans les ' ' ça additionne
+
+--- Comparison operators ---
+
+===: returns true if both operands have exactly the same value. Else it returns false.
+!=: returns true if both operands don't have the same value.
+>: returns true if the first operand is strictly greater than the second one.
+<: returns true if the first operand is strictly lower than the second one.
+>=: returns true if the first operand is greater than or equal to the second one.
+<=: returns true if the first operand is lower than or equal to the second one.
+
+--- Logic operators ---
+
+&&: returns true if both operands are true.
+||: returns true if any of the operands are true.
+!: only takes one operand. Inverse the value of its operand. If it was true it becomes false. If it was false it becomes true.
+
+--- Combine operators ---
+
+(a > 2) && (b < 4): returns true only if a is greater than 2 and b is lower than 4.
+(x >= y) || (y === z): returns true only if x is greater than or equal to y or y is equal to z.
+!(a < 2): returns true only if a is not lower than 2. This is the same thing than writing a >= 2.
+(!(a < b) && (x > z)) || !(i === j): huu... try to guess ! */
