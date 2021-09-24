b60280c454734e833987c4048de54459cdf168ac
Bruno_P <presti.bruno88@gmail.com>
2021-09-24 15:51:37 +0200

    Initialization to js

---
+++ Control-structures/2.6.js
@@ -0,0 +1,25 @@
+const readlineSync = require("readline-sync");
+
+let number = parseInt(readlineSync.question("Enter a number between 1 and 7 "));
+
+if (number === 1) {
+  console.log("Monday");
+}
+if (number === 2) {
+  console.log("Tuesday");
+}
+if (number === 3) {
+  console.log("Wednesday");
+}
+if (number === 4) {
+  console.log("Thursday");
+}
+if (number === 5) {
+  console.log("Friday");
+}
+if (number === 6) {
+  console.log("Saturday");
+}
+if (number === 7) {
+  console.log("Sunday");
+}
