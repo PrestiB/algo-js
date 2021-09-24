0cabbf2cb22172eeb71fac56a4d9c5ef2eb00e1c
Bruno_P <presti.bruno88@gmail.com>
2021-09-24 12:08:09 +0200

    Initialization to js

--- Control-structures/2.2.js
+++ Control-structures/2.2.js
@@ -1,7 +1,23 @@
+/*const readlineSync = require("readline-sync");
+let minAge = readlineSync.question("Give me a number");
+let currentAge = readlineSync.question("Give me another one");
+let maxAge = readlineSync.question("Give me a last one");
+
+if ((currentAge > minAge) && (currentAge < maxAge)) {
+  console.log("It's ok !");
+} else {
+  console.log("Error !");
+}*/
+
 const readlineSync = require("readline-sync");
-let minAge = readlineSync.question("What is the minimum age to participate?");
-let maxAge = readlineSync.question("What is the maximumn age to participate?");
-let currentAge = readlineSync.question("What is your current age?");
-if (minAge >= 18 && maxAge <= 40) {
-  console.log("You're welcome at the party");
+let minAge = readlineSync.question("Give me a number");
+let currentAge = readlineSync.question("Give me another one");
+let maxAge = readlineSync.question("Give me a last one");
+
+if (currentAge > minAge && currentAge < maxAge) {
+  console.log("It's ok !");
+} else if (minAge > maxAge) {
+  console.log("Error !");
+} else {
+  console.log("Sorry but you don't understand");
 }
