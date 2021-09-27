a3a7e2e7adb7f10d3db441de3986cc0dd76f0966
Bruno_P <presti.bruno88@gmail.com>
2021-09-27 10:52:31 +0200

    Initialization to js

---
+++ Arrays/3.3.js
@@ -0,0 +1,10 @@
+let arr1 = [8, 8, 9, 23, 9];
+let arr2 = [];
+for (let elem of arr1) {
+  arr2.push(elem);
+}
+console.log(arr2);
+
+/*let arr1 = [8, 8, 9, 23, 9]
+let arr2 = arr1
+console.log(arr2)*/
