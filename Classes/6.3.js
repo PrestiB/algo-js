bf129bab4d04f15e1c4af111a03075a52ecbb018
Bruno_P <presti.bruno88@gmail.com>
2021-09-29 10:31:43 +0200

    Initialization to js

---
+++ Classes/6.3.js
@@ -0,0 +1,24 @@
+class Cat {
+  constructor(name) {
+    this.name = name;
+    this.greeting = "Miaou! ";
+  }
+  sayHello() {
+    return this.greeting + "I'm " + this.name;
+  }
+}
+class Dog {
+  constructor(name) {
+    this.name = name;
+    this.greeting = "Waouf! ";
+  }
+  sayHello() {
+    return this.greeting + "I'm " + this.name;
+  }
+}
+
+let newDog = new Dog("Pixel");
+let newCat = new Cat("Satanas");
+
+console.log(newDog.sayHello());
+console.log(newCat.sayHello());
